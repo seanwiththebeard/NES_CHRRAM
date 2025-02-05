86b8a2adc8cd35f809bd1ced22a0a71e90aa428f
seanwiththebeard <42120363+seanwiththebeard@users.noreply.github.com>
2025-02-05 09:25:46 -0500

    updated from 8bitworkshop.com

--- chr_generic.s
+++ chr_generic.s
@@ -1,6 +1,2 @@
-
-;;{w:8,h:8,bpp:1,count:256,brev:1,np:2,pofs:8};;
-;.segment "CHARS"
-;.incbin "tileset.bin"
-;;
-;/* use block comment to map a subset of data to sprite bitmaps */
+.segment "CODEB_0"
+.incbin "tileset.bin"
